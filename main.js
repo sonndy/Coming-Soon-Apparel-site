34953040db0f9c017a5a53ed6b020dff3288c47e
sonndy <123dykai@gmail.com>
2021-07-22 12:55:00 -0400

    add error message to check validity of email address

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const emailInput = document.getElementById("email");
     const submitEmailBtn = document.querySelector(".emailBtn");
     const errorIcon = document.querySelector(".error");
+    const inputContainer = document.querySelector(".buttonInput");
 
     function validateEmail(email) {
         const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
@@ -20,8 +21,23 @@ window.addEventListener("DOMContentLoaded", () => {
                 if(validEmail === false) {
                     errorIcon.classList.add("shown");
                     //appending the error message under the input button
+                    const errorMessage = document.createElement("div");
+                    errorMessage.classList.add("error-message");
+                    // console.log(errorMessage.innerText)
+                    errorMessage.textContent = "Please provid a valid email address";
+                    
+                    inputContainer.parentNode.insertBefore(errorMessage, inputContainer.nextSibling)
+                    // inputContainer.append(errorMessage);
                 } else {
-                    errorIcon.classList.remove("shown")
+                    errorIcon.classList.remove("shown");
+
+                    let checkError = document.querySelector(".error-message");
+                    if(checkError !== null)
+                    {
+                        checkError.remove();
+                    }
+                    // console.log(checkError);
+                    
                 }
 
             })
